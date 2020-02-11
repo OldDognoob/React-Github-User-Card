23443973e2e13075ad8b26a3804e8b23ba57d061
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-11 18:14:10 +0200

    set up follwercard

---
+++ my-app/src/components/FollowerCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FollowerCard extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default FollowerCard;
